0853ce146b18d62b5b9afb4e8501def63d1edb8c
Ted Gusek <theohgusek@gmail.com>
2024-02-01 13:26:28 -0500

    Headshot Icon and formatting landing page

--- app/page.tsx
+++ app/page.tsx
@@ -49,25 +49,15 @@ export default function Home() {
         />
       </div>
         {/* <div className="fixed bottom-0 left-0 flex h-48 w-25 items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-13 lg:bg-none"> */}
-        <div className="fixed bottom-0 left-0 flex h-48 w-25 items-end justify-center lg:static lg:h-auto lg:w-13 lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex flex-col h-48 w-25 items-end justify-center lg:static lg:h-auto lg:w-13 lg:bg-none">
           <a
-            // className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             className=" gap-2 p-2 text-2xl lg:text-5xl lg:p-0"
-            // href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            // target="_blank"
-            // rel="noopener noreferrer"
           >
             Theo Gusek
-            {/* By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            /> */}
           </a>
+          <text className=" gap-2 p-2 text-.5xl lg:text-1xl lg:p-0">
+            Software Engineer BK, NY
+          </text>
         </div>
       </div>
 
