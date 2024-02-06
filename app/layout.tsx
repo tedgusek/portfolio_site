acfa74e816eaacc51e24f4e08d95a3307b932d92
Ted Gusek <theohgusek@gmail.com>
2024-02-05 21:59:53 -0500

    nav bar is working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+
+// import Footer from '@/components/Footer';
+// import ThemeSwitch from '@/components/ThemeSwitch';
+import ActiveSectionContextProvider from '@/context/ActiveSectionContextProvider';
+// import ThemeContextProvider from '@/context/ThemeContextProvider';
+// import { ClerkProvider } from '@clerk/nextjs';
+// import { GoogleAnalytics } from '@next/third-parties/google';
+// import { Analytics } from '@vercel/analytics/react';
+// import { SpeedInsights } from '@vercel/speed-insights/next';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,14 +21,45 @@ export const metadata: Metadata = {
   description: "A page to share a couple projects and offer services",
 };
 
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   );
+// }
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${inter.className} sm:pt-38 relative bg-gray-50 pt-28 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+      >
+        {/* Display two subtle pops of color to the top of the page to add some visual interest */}
+        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
+        <div className="xl:left=[-15rem] absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] 2xl:left-[-5rem] "></div>
+        {/* <ClerkProvider> */}
+          {/* <ThemeContextProvider> */}
+            <ActiveSectionContextProvider>
+              {children}
+              {/* <Footer /> */}
+            </ActiveSectionContextProvider>
+            {/* <ThemeSwitch /> */}
+          {/* </ThemeContextProvider> */}
+        {/* </ClerkProvider> */}
+        {/* <Analytics /> */}
+        {/* <SpeedInsights /> */}
+        {/* {GOOGLE_ANALYTICS_ID ? (
+          <GoogleAnalytics gaId={GOOGLE_ANALYTICS_ID} />
+        ) : null} */}
+      </body>
     </html>
   );
 }
