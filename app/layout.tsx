30c71349d31b85ae36d0917f73a606a04e9f8093
Ted Gusek <theohgusek@gmail.com>
2024-02-06 23:27:51 -0500

    in the midst of adding a mdx file intro and need to sort out the md loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import ActiveSectionContextProvider from '@/context/ActiveSectionContextProvider
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+// import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,12 +39,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="!scroll-smooth">
+    <html lang="en" >
+      {/* <link rel="icon" href="/favicon.ico" sizes="any" /> */}
       <body
-        className={`${inter.className} sm:pt-38 relative bg-gray-50 pt-28 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={`${inter.className} sm:pt-38 relative bg-gray-50 pt-8 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 `}
       >
         {/* Display two subtle pops of color to the top of the page to add some visual interest */}
-        {/* <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div> */}
+        <div className="absolute left-[-30rem] top-[0rem] -z-10 h-[15.25rem] w-[5.25rem] rounded-full bg-[#008080] blur-[10rem] dark:bg-[#008080] dark:bg-opacity-75 sm:w-[68.75rem]"></div>
         {/* <div className="xl:left=[-15rem] absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] 2xl:left-[-5rem] "></div> */}
         {/* <ClerkProvider> */}
           {/* <ThemeContextProvider> */}
