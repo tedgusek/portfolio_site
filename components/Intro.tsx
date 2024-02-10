c3abf573751f3eec8eb04e2b0342e7c82d00f1a1
Ted Gusek <theohgusek@gmail.com>
2024-02-10 00:08:30 -0500

    one more save

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -76,14 +76,14 @@ function Home() {
           </TrackedLink> */}
 
           {/* LinkedIn Button */}
-          {/* <Link
+          <Link
             href="https://www.linkedin.com/in/hamlinjohn/"
             className="flex items-center justify-center gap-2 rounded-full border border-black/10 bg-white p-4  text-[1.35rem] text-gray-900 transition hover:scale-[1.15] hover:text-gray-900 focus:scale-[1.15] active:scale-105 dark:bg-white/10 dark:text-white/60"
             target="_blank"
           >
             <BsLinkedin />
             <span className="text-lg sm:hidden">LinkedIn</span>
-          </Link> */}
+          </Link>
 
           {/* GitHub Button */}
           {/* <Link
